393680489bbec6294358cc9c81b848176bad7e31
Siddharth Shinde <siddharthshinde9476@gmail.com>
2026-04-14 23:49:10 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,14 @@
 export const submitApplication = async (payload) => {
-  const res = await fetch(https://msme-lending-decision-system.onrender.com/api/decision, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(payload)
-  });
+  const res = await fetch(
+    "https://msme-lending-decision-system.onrender.com/api/decision",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    }
+  );
 
   if (!res.ok) {
     const err = await res.json();
